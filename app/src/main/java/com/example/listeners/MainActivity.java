89a4dfcec0a0d2ea30eff1ef08dc8a7dc8debebd
dg5188bs <dg5188@bs.amalnet.k12.il>
2022-10-28 14:06:46 +0300

    sofi 2

--- app/src/main/java/com/example/listeners/MainActivity.java
+++ app/src/main/java/com/example/listeners/MainActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button red,blue,results;
     int i;
